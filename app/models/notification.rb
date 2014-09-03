9fa0ae5a032194a879d0eeafa6e804786f17cb4a
lrsnider <lisarsnider@gmail.com>
2014-09-03 19:42:23 -0400

    finished hmwk w05/d03

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,18 +4,32 @@ require_relative '../../credentials'
 class Notification
   attr_accessor :to_number
 
-  def initialize
+  def initialize(options = {})
+    self.to_number = options[:to_number]
     account_sid = Credentials.credentials[:twilio_account_sid]
     auth_token = Credentials.credentials[:twilio_account_token]
     @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
   def text
-    # Sends a text to your phone number
+    @client.account.messages.create(
+      :from => "+12027514380",
+      :to => "+12402325764",
+      :body => random_compliment
+    )
   end
 
   def random_compliment
-    # Generates a random compliment to send to the textee
+
+    compliments = [
+    "Your app is fantastic!",
+    "High five = ^5",
+    "What a phenomenal view!",
+    "Your controller is on the ball!",
+    "May you develop swiftly and with godspeed."
+    ]
+
+    compliment = compliments.sample  
   end
 
 end
